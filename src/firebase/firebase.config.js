5db7142f29729db403199ee515022c567e898836
TanvirHassanSayem <sayemt672@gmail.com>
2025-03-09 17:20:42 +0600

    ee

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyBHSlGdWVHoTRp5n8kkqVVHE-j02k_Rzqc",
-  authDomain: "hoichoidev-f27a5.firebaseapp.com",
-  projectId: "hoichoidev-f27a5",
-  storageBucket: "hoichoidev-f27a5.appspot.com",
-  messagingSenderId: "579988425072",
-  appId: "1:579988425072:web:29fd71348b5d7ab92767b5",
-  measurementId: "G-VNL3TW7L1V"
+  apiKey: "AIzaSyCHdxJYmjsUiVP60sv9W7PLveh-4xJj48Q",
+  authDomain: "winter-clothing-donation-d04bf.firebaseapp.com",
+  projectId: "winter-clothing-donation-d04bf",
+  storageBucket: "winter-clothing-donation-d04bf.firebasestorage.app",
+  messagingSenderId: "248098988359",
+  appId: "1:248098988359:web:de2d72add60e0e865e2c62"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-export default app;
+const auth = getAuth(app);
 
+export default auth;
