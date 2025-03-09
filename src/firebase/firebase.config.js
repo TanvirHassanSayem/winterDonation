9bad1e7c2ebd4a4c210a28ab045a56625a11f975
TanvirHassanSayem <sayemt672@gmail.com>
2025-03-09 17:33:40 +0600

    11

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // import dotenv from 'dotenv';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // dotenv.config();
@@ -19,16 +19,15 @@ import { getAnalytics } from "firebase/analytics";
 //     measurementId: import.meta.env.VITE_MEASUREMENT_ID
 //   };
 const firebaseConfig = {
-  apiKey: "AIzaSyBHSlGdWVHoTRp5n8kkqVVHE-j02k_Rzqc",
-  authDomain: "hoichoidev-f27a5.firebaseapp.com",
-  projectId: "hoichoidev-f27a5",
-  storageBucket: "hoichoidev-f27a5.appspot.com",
-  messagingSenderId: "579988425072",
-  appId: "1:579988425072:web:29fd71348b5d7ab92767b5",
-  measurementId: "G-VNL3TW7L1V"
+  apiKey: "AIzaSyCHdxJYmjsUiVP60sv9W7PLveh-4xJj48Q",
+  authDomain: "winter-clothing-donation-d04bf.firebaseapp.com",
+  projectId: "winter-clothing-donation-d04bf",
+  storageBucket: "winter-clothing-donation-d04bf.firebasestorage.app",
+  messagingSenderId: "248098988359",
+  appId: "1:248098988359:web:de2d72add60e0e865e2c62"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 export default app;
