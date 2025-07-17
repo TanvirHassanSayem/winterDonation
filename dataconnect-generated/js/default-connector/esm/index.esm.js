c9e38f42bf9befab13b490dc5114d55cffd16e95
TanvirHassanSayem <sayemt672@gmail.com>
2025-07-18 05:36:30 +0600

    ex1

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'winterdonation',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'winterdonation',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
